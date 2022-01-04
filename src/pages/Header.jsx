63b3860d8bd10c1271717e7881aff86035f2f446
natnaelh14 <natnaelh14@github.com>
2022-01-04 15:22:03 -0500

    update gh pages

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,21 +1,50 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import logo from './logo.png';
 
 const Header = () => {
   const dashboards = useSelector((state) => state.dashboards);
   const { dashboard } = dashboards;
-  const boards = dashboard.filter((board) => board !== null).filter((board) => board !== undefined)
+  const boards = dashboard
+    .filter((board) => board !== null)
+    .filter((board) => board !== undefined);
 
   return (
-    <Nav style={{backgroundColor: '#00d2b1'}} className='justify-content-center' activeKey='/home'>
-      <Nav.Item style={{ margin: '1.5rem', textDecoration: 'none' }} >
-        <NavLink style={{ color: 'black', fontSize: '2rem', fontWeight: 'bold',textDecoration: 'none' }} to='/'>{`Dashboard (${boards?.length ? boards?.length : 0})`}</NavLink>
+    <Nav
+      style={{ backgroundColor: '#00d2b1' }}
+      className='justify-content-flex-start'
+      activeKey='/home'
+    >
+        <img alt='logo' style={{ width: 125, height: 125 }} src={logo} />
+        <div style={{ marginLeft: 'auto', display: 'flex', flexDirection: 'row'}}>
+               <Nav.Item style={{ margin: '1.5rem', textDecoration: 'none' }}>
+        <NavLink
+          style={{
+            color: 'black',
+            fontSize: '2rem',
+            fontWeight: 'bold',
+            textDecoration: 'none',
+          }}
+          to='/'
+        >{`Dashboard (${boards?.length ? boards?.length : 0})`}</NavLink>
       </Nav.Item>
-      <Nav.Item style={{ margin: '1.5rem'}}>
-        <NavLink style={{ color: 'black', fontSize: '2rem', fontWeight: 'bold', textDecoration: 'none' }} to='/create'>Create Dashboard</NavLink>
-      </Nav.Item>
+      <Nav.Item style={{ margin: '1.5rem' }}>
+        <NavLink
+          style={{
+            color: 'black',
+            fontSize: '2rem',
+            fontWeight: 'bold',
+            textDecoration: 'none',
+          }}
+          to='/create'
+        >
+          Create Dashboard
+        </NavLink>
+      </Nav.Item> 
+        </div>
+
     </Nav>
   );
 };
